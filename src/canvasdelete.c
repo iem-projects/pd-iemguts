d3adfa812e9cc4ccaa595791bffe3cb90b614d6b
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2016-01-19 18:41:20 +0100

    close top-level patch if matched exactly

--- src/canvasdelete.c
+++ src/canvasdelete.c
@@ -16,7 +16,7 @@
  ******************************************************/
 
 
-/* 
+/*
  * this object deletes itself (or the specified parent canvas) when banged
  */
 
@@ -43,7 +43,14 @@ static void canvasdelete_doit(t_canvasdelete *x)
   clock_free(x->x_clock);
   x->x_clock=NULL;
 
-  glist_delete(x->x_glist, x->x_gobj);
+  if(x->x_glist)
+    glist_delete(x->x_glist, x->x_gobj);
+  else {
+    t_atom ap[1];
+    SETFLOAT(ap, 1);
+    typedmess(x->x_gobj, gensym("menuclose"), 1, ap);
+  }
+
 
   canvas_resume_dsp(dspstate);
 }
@@ -54,8 +61,7 @@ static void canvasdelete_bang(t_canvasdelete *x)
     pd_error(x, "deletion already scheduled");
     return;
   }
-
-  if(NULL==x->x_glist || NULL==x->x_gobj)return;
+  if(NULL==x->x_gobj)return;
 
   x->x_clock=clock_new(x, (t_method)canvasdelete_doit);
   clock_delay(x->x_clock, 0);
@@ -68,7 +74,7 @@ static void *canvasdelete_new(t_floatarg f)
   t_canvas *canvas=(t_canvas*)glist_getcanvas(glist);
   t_gobj*obj=(t_gobj*)x;
   int depth=(int)f;
-  
+
   if(depth<0)depth=0;
 
   while(depth && canvas) {
@@ -77,9 +83,12 @@ static void *canvasdelete_new(t_floatarg f)
     depth--;
   }
 
-  x->x_glist = canvas;
-  x->x_gobj=obj;
-  
+  x->x_glist = x->x_gobj = NULL;
+  if (!depth) {
+    x->x_glist = canvas;
+    x->x_gobj=obj;
+  }
+
   x->x_clock=NULL;
 
   return (x);
@@ -95,8 +104,8 @@ static void canvasdelete_canvasmethods(void);
 void canvasdelete_setup(void)
 {
   iemguts_boilerplate("[canvasdelete] - delete message for the canvas", 0);
-  canvasdelete_class = class_new(gensym("canvasdelete"), 
-                                 (t_newmethod)canvasdelete_new, (t_method)canvasdelete_free, 
+  canvasdelete_class = class_new(gensym("canvasdelete"),
+                                 (t_newmethod)canvasdelete_new, (t_method)canvasdelete_free,
                                  sizeof(t_canvasdelete), 0,
                                  A_DEFFLOAT, 0);
   class_addbang(canvasdelete_class, (t_method)canvasdelete_bang);
@@ -108,7 +117,7 @@ void canvasdelete_setup(void)
 /* 'delete' message for the canvas */
 static int canvas_delete_docb(t_glist*glist, int index) {
   /* this will crash Pd if the object to be deleted is on the stack
-   * workarounds: 
+   * workarounds:
    *   - use a clock (see above)
    *   - finally fix this in Pd
    */
@@ -155,6 +164,5 @@ static void canvasdelete_canvasmethods(void) {
     verbose(0, "adding 'delete' method to canvas");
     class_addmethod(canvas_class, (t_method)canvas_delete_cb, gensym("delete"), A_GIMME, 0);
   }
-
 }
 
